f9e0ecd1152fee8cf88262918f854c97c917b91d
Abyaya Khanal <abyayabkhanal@gmail.com>
2021-07-27 04:26:46 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Todo from './components/Todo'
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>My Todods</h1>
+      <Todo text='learn react' />
+      <Todo text='now' />
+      <Todo text='quick' />
+      
+    </div>
+  );
+}
+
+export default App;
